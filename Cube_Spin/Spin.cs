43914bd60b3c0b1a4a368738057e7a9dbf092930
Jason Sutter <jasonsutter87@gmail.com>
2016-05-07 12:07:18 -0700

    Added CubeSpine unity file

---
+++ Cube_Spin/Spin.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+
+	public float speed = 10f;
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+        
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+	
+	}
+}
